30c034710e53afad2b93730dee9e2147816fe0a0
Francesco Della Morte <admin@fdm91.net>
2025-04-07 14:10:27 +0200

    Footer aggiornato

--- footer.php
+++ footer.php
@@ -4,7 +4,15 @@
 
     <footer class="footer bg-dark text-light text-center border-top border-dark py-2">
         <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> MyFDM91. Tutti i diritti riservati.</p>
+            <p>&copy; <?php echo date('Y'); ?> MyFDM91. Tutti i diritti riservati.
+            <?php
+                $version = trim(@file_get_contents("VERSION"));
+                if ($version) {
+                    echo ' <a href="https://github.com/fdm91github/myfdm91/releases/tag/' . $version . '">Versione ' . $version . '</a>';
+                }
+            ?>
+            </p>
         </div>
     </footer>
 </body>
+
