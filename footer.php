c9b28cd8217815c25d2ab5a68557946747f39d7c
Francesco Della Morte <admin@fdm91.net>
2025-04-07 14:25:59 +0200

    Footer aggiornato con note di rilascio

--- footer.php
+++ footer.php
@@ -4,11 +4,16 @@
 
     <footer class="footer bg-dark text-light text-center border-top border-dark py-2">
         <div class="container">
-            <p>&copy; <?php echo date('Y'); ?> MyFDM91. Tutti i diritti riservati.
+            <p>&copy; <?php echo date('Y'); ?> MyFDM91. Tutti i diritti riservati
             <?php
-                $version = trim(@file_get_contents("VERSION"));
+                // Percorso assoluto relativo alla root del progetto
+                $versionFile = $_SERVER['DOCUMENT_ROOT'] . "/VERSION";
+                $version = trim(@file_get_contents($versionFile));
+
                 if ($version) {
-                    echo ' <a href="https://github.com/fdm91github/myfdm91/releases/tag/' . $version . '">Versione ' . $version . '</a>';
+                    // Link alla release GitHub
+                    $githubUrl = "https://github.com/fdm91github/myfdm91/releases/tag/" . $version;
+                    echo ' - v' . $version . ' (<a style="color:white;" href="' . $githubUrl . '" target="_blank" rel="noopener">Note di rilascio</a>)';
                 }
             ?>
             </p>
